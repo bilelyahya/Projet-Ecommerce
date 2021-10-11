420d034ea770802aaa50c73b2ba526b90ba9318d
bilel yahya <yahya.bilel@gmail.com>
2021-10-11 11:49:16 +0100

    mern

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import productRouter from './routers/productRouter.js';
+import userRouter from "./routers/userRouter.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect( "mongodb://localhost:27017/amazona", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateIndex: true,
+},console.log("DB connected")
+
+).catch(error => handleError(error));
+
+app.use("/api/users", userRouter);
+app.use('/api/products', productRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server at http://localhost: ${port}`);
+});
